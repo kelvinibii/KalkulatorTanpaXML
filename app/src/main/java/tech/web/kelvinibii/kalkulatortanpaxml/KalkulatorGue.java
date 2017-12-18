db0d1924ea8989c779e4137f3e34efd3aad6842e
Kelvin Du <kelvin.fto@gmail.com>
2017-12-18 13:20:01 +0700

    Add members name

--- app/src/main/java/tech/web/kelvinibii/kalkulatortanpaxml/KalkulatorGue.java
+++ app/src/main/java/tech/web/kelvinibii/kalkulatortanpaxml/KalkulatorGue.java
@@ -9,6 +9,14 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 /**
+ * Tugas kalkulator WT3
+ * Anggota kelompok:
+ * -Kelvin - 56140275
+ * -Harfi  - 55140131
+ * -Jemmy  - 52140071
+ * -Mikkel - 50140035
+ * -Samuel - 57140122
+ *
  * Created by KelvinDu on 12/12/2017.
  */
 
